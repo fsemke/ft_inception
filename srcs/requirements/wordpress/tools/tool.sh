10ad24233b998466ed7824584550c67e8f4d95f2
florian <florian@Debian.myguest.virtualbox.org>
2023-09-13 15:09:44 +0200

    prettier

--- srcs/requirements/wordpress/tools/tool.sh
+++ srcs/requirements/wordpress/tools/tool.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ -f "/var/www/wordpress/wp-config.php" ]; then
 	echo "Wordpress already exist"
-	exec /usr/sbin/php-fpm81 -F
 else
 	wp core download --allow-root
 	wp core config --dbname="$DB_DB" --dbuser="$DB_USER" --dbpass="$DB_PW" --dbhost="$DB_HOST:$DB_PORT" --dbprefix='wp_' --allow-root
 	wp core install --url="$DOMAIN_NAME" --title="Welcome to the website from florian" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN@adminmail.de" --allow-root
 	wp user create $WP_USER $WP_USER@user.com --role='subscriber' --user_pass="$WP_PW" --allow-root
-	exec /usr/sbin/php-fpm81 -F
 fi
+exec "$@"
