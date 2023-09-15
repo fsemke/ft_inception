27dab1b47f49d85f473eb6afcbef141442aa4c91
florian <florian@Debian.myguest.virtualbox.org>
2023-09-15 16:37:28 +0200

    added and changed env

--- srcs/requirements/mariadb/conf/setup.sh
+++ srcs/requirements/mariadb/conf/setup.sh
@@ -1,14 +1,14 @@
 #!bin/sh
 
-if [ ! -d "/var/lib/mysql/$DB_DB" ]; then
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
 
 	# Initialize the database
 	mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
 	# replace variables
-	sed -i "s/DB_DB/$DB_DB/g" /var/mariadb/db_create.sql
-	sed -i "s/DB_USER/$DB_USER/g" /var/mariadb/db_create.sql
-	sed -i "s/DB_PW/$DB_PW/g" /var/mariadb/db_create.sql
+	sed -i "s/DB_NAME/$DB_NAME/g" /var/mariadb/db_create.sql
+	sed -i "s/DB_USER_PW/$DB_USER_PW/g" /var/mariadb/db_create.sql
+	sed -i "s/DB_USER_NAME/$DB_USER_NAME/g" /var/mariadb/db_create.sql
 	sed -i "s/DB_ROOT_PW/$DB_ROOT_PW/g" /var/mariadb/db_create.sql
 fi
 
--- srcs/requirements/wordpress/tools/tool.sh
+++ srcs/requirements/wordpress/tools/tool.sh
@@ -4,8 +4,13 @@ if [ -f "/var/www/wordpress/wp-config.php" ]; then
 	echo "Wordpress already exist"
 else
 	wp core download --allow-root
-	wp core config --dbname="$DB_DB" --dbuser="$DB_USER" --dbpass="$DB_PW" --dbhost="$DB_HOST:$DB_PORT" --dbprefix='wp_' --allow-root
-	wp core install --url="$DOMAIN_NAME" --title="Welcome to the website from florian" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN@adminmail.de" --allow-root
-	wp user create $WP_USER $WP_USER@user.com --role='subscriber' --user_pass="$WP_PW" --allow-root
+	echo "wp core downloaded"
+	wp core config --dbname="$DB_NAME" --dbuser="$DB_USER_NAME" --dbpass="$DB_USER_PW" --dbhost="$DB_HOST:$DB_PORT" --dbprefix='wp_' --allow-root
+	echo "WP db configured"
+	wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PW" --admin_email="$WP_ADMIN_MAIL" --allow-root
+	echo "wp installed"
+	echo "wp user create $WP_USER_NAME $WP_USER_MAIL --role='subscriber' --user_pass="$WP_USER_PW" --allow-root"
+	wp user create $WP_USER_NAME $WP_USER_MAIL --role='subscriber' --user_pass="$WP_USER_PW" --allow-root
+	echo "wp user added"
 fi
 exec "$@"
